7264b521d970f7cf4f61b2517f46a01cd8256a2c
Andrii Protas <andreyprotas5@gmail.com>
2023-09-13 22:06:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace mpt_lab_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 12;
+            int number2 = 4;
+
+            int sum = Sum(number1, number2);
+            int square = Square(sum);
+        
+            Console.WriteLine("\nThe square of the sum of {0} and {1} is {2}", number1, number2, square);
+        }
+        
+        /// <summary>
+        /// Method to calculate the square of a number.
+        /// </summary>
+        /// <param name="number">Input number.</param>
+        /// <returns>The square of the input number.</returns>
+        static int Square(int number)
+        {
+            return number * number;
+        }
+        
+        /// <summary>
+        /// Method to calculate the sum of two numbers.
+        /// </summary>
+        /// <param name="number1">The first number.</param>
+        /// <param name="number2">The second number.</param>
+        /// <returns>The sum of the two input numbers.</returns>
+        static int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
